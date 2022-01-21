50f0865676cd26a5d1a72027ee7dd61d4735803c
“santhoshi123654” <santhoshi.badam@gmail.com>
2022-01-21 23:28:37 +0530

    Added:2D Array program

---
+++ Functional_Examples/2DArray.php
@@ -0,0 +1,9 @@
+<?php
+$numbers = array (
+    array(10.6,22,true),
+    array(16.6,15,0)
+  );
+    
+  echo $numbers[0][0]." ".$numbers[0][1]." ".$numbers[0][2]."\n";
+  echo $numbers[1][0]." ".$numbers[1][1]." ".$numbers[1][2]."\n";
+?>
